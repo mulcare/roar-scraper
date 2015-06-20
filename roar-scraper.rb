739d8a2834147b8c55f4362e7058ae7e8925bde8
Ross Mulcare <ross.mulcare@gmail.com>
2015-06-20 22:11:31 +0200

    working prototype for a single scenario

---
+++ roar-scraper.rb
@@ -0,0 +1,28 @@
+require 'open-uri'
+require 'nokogiri'
+require 'mechanize'
+require 'json'
+
+#use mechanize to follow all scenario name links here:
+#http://www.jrvdev.com/ROAR/VER1/RecordByName.asp
+
+#use nokogiri to scrape individual scenario information
+doc = Nokogiri::HTML(open("http://www.jrvdev.com/ROAR/VER1/ShowScenario.asp?ScenarioID=125"))
+
+scenario_name = doc.at_css("td.ScenarioName").inner_html
+scenario_id = doc.at_css("td.ScenarioNumber").inner_html
+
+#wins and losses are buried in tables so we must pull down all <td> elements
+winloss = doc.xpath("//td")
+
+side_a_wins = winloss[18].inner_html
+side_b_wins = winloss[19].inner_html
+
+#side names are buried as well
+sides = doc.xpath("//th")
+
+side_a_name = sides[0].inner_html.chomp(" wins")
+side_b_name = sides[1].inner_html.chomp(" wins")
+
+#output (will be JSON eventually)
+puts "#{scenario_name} (#{scenario_id}): #{side_a_wins} #{side_a_name} wins / #{side_b_wins} #{side_b_name} wins"
